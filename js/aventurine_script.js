0c3d1ebd5f540a0ceed6793e3b500db728718597
haynako0 <hayspanibagongaltnanaman@gmail.com>
2024-04-12 08:42:16 +0800

    removed overlay, turned to sound button

--- js/aventurine_script.js
+++ js/aventurine_script.js
@@ -1,19 +1,21 @@
-function playVideoWithSound() {
-    var video = document.getElementById('video-background');
-    var playButton = document.getElementById('playButton');
-    var overlay = document.getElementById('overlay');
-    var gradeTitle = document.querySelector('.grade-title');
-
-    // Play video with sound
-    video.muted = false;
-    video.play();
-
-    // Add fade-out animation class to the overlay
-    overlay.classList.add('fade-out');
-
-    // Hide overlay and play button after animation completes
+window.onload = function() {
+    var chatBubble = document.getElementById('chatBubble');
     setTimeout(function() {
-        overlay.style.display = 'none';
-        gradeTitle.style.top = '50%'; // Move the grade title to the middle of the page
-    }, 1000); // Adjust duration to match the fade-out animation duration
+        chatBubble.style.display = 'none';
+    }, 8000); // Hide chat bubble after 8 seconds
+};
+
+function toggleSound() {
+    var video = document.getElementById('video-background');
+    var soundButton = document.getElementById('soundButton');
+    var chatBubble = document.getElementById('chatBubble');
+
+    if (video.muted) {
+        video.muted = false;
+        soundButton.innerHTML = '<img src="img/kakavasha.webp" alt="Sound Button">';
+    } else {
+        video.muted = true;
+        soundButton.innerHTML = '<img src="img/kakavasha.webp" alt="Mute Button">';
+        chatBubble.style.display = 'block';
+    }
 }
